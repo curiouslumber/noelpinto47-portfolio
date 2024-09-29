121abc9ed8fab20fdbae8e9b5d634790dbaa7095
Noel Pinto <noelpinto47@gmail.com>
2024-09-29 21:17:43 +0530

    updated

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <Box>
             <Typography>
-                Hello World
+                Hello World 123 
             </Typography>
         </Box>
     )
